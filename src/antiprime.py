ab4b093b5fecc69b29d89567ad6bd71f1c517a93
Adrià Piñol <adria.pinol01@estudiant.upf.edu>
2024-10-21 17:40:28 +0200

    First attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -2,16 +2,38 @@
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
 def main() :
+
+	i=1
+	x= int(input("Enter a positive integer number: "))
+	sum1=0
+	y=x-1
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
+	while i<=x:
+		if x% i==0:
+			sum1= sum1+u
+		i=i+1
+	while i<=x:
+		i=1
+		while i<=y:
+			if y%i==0:
+				sum2=sum2 +u
+			i=i+1
+		if sum2>=sum1:
+			print("No anti-prime")
+			y=0
+		else:
+			y=y-1
+			sum2=0
+		if sum2<sum1:
+			print("anti-prime")
 
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
 	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
 	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
-	return("anti-prime")
+	## "anti-prime" or "not anti-prime
 
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
